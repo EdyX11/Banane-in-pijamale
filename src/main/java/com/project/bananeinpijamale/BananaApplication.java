070564e1bef27099359c50870ab418c486af840c
SuperxJoker <oaidaalexia@yahoo.com>
2022-04-15 22:49:22 +0300

    doar background gif

--- src/main/java/com/project/bananeinpijamale/BananaApplication.java
+++ src/main/java/com/project/bananeinpijamale/BananaApplication.java
@@ -12,8 +12,8 @@ public class BananaApplication extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("interface-view.fxml"));
-        primaryStage.setTitle("Application");
-        primaryStage.setScene(new Scene(root, 600, 488));
+        //primaryStage.setTitle("Application");
+        primaryStage.setScene(new Scene(root, 1229, 674));
         primaryStage.show();
     }
 
--- src/main/java/com/project/bananeinpijamale/InterfaceController.java
+++ src/main/java/com/project/bananeinpijamale/InterfaceController.java
@@ -1,19 +1,45 @@
 package com.project.bananeinpijamale;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class InterfaceController implements Initializable {
     @FXML
-    private Button button;
+    private Button startButton;
+    @FXML
+    private BorderPane scenePane1;
+    @FXML
+    private Button exitButton;
+    @FXML
+    private ImageView imageview;
+
+
+
+    Stage stage;
+
+    public void exit(ActionEvent event){
+        stage = (Stage) scenePane1.getScene().getWindow();
+        System.out.println("U closed the app");
+        stage.close();
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+       
 
     }
+
+
 }
